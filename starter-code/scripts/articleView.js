b0e2b58075832927561ebdbb03bca41292b1c18c
Renard Burnett <renardburnett@Renards-MacBook-Pro.local>
2017-07-12 18:26:52 -0700

    oops

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -92,7 +92,7 @@ articleView.handleMainNav = function() {
   //       Clicking any .tab element should hide all the .tab-content sections, and then reveal the
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
-  //       data available to you on the .tab element that was clicked.
+  //       data available to you on the .tab element that was clicked
 
 
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
