5e4c951a8c6b46075ba1775a98c0a7868f071d6c
Drew Biehle <drew.biehle@gmail.com>
2017-07-13 10:21:39 -0700

    Read-on now shows what it should and disappears

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -96,7 +96,11 @@ articleView.setTeasers = function() {
   //       "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   //       Ideally, we'd attach this as just 1 event handler on the #articles section, and let it
   //       process any .read-on clicks that happen within child nodes.
-
+  $('.read-on').click(function(event){
+    event.preventDefault();
+    $('.article-body *:nth-of-type(n+2)').show();
+    $('.read-on').hide();
+  })
   // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
 
 };
@@ -107,4 +111,5 @@ $(document).ready(function() {
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
+  articleView.setTeasers();
 })
