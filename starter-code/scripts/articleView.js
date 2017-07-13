51e2ff96cffd5b6017802ddb2ad8d6164127bfed
dmuldrew <dmuldrew@gmail.com>
2017-07-12 17:42:14 -0700

    handled the blank author case

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -48,6 +48,7 @@ articleView.handleAuthorFilter = function() {
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
+      $('article').not('.template').fadeIn('slow');
     }
     $('#category-filter').val('');
   });
