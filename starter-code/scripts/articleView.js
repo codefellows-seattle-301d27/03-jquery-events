5eac8384668320f6728e044ac64713cdfd5340bf
Rami Bououni <rami.bououni@gmail.com>
2017-07-12 17:22:12 -0700

    aded the filter author

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -36,10 +36,15 @@ articleView.handleAuthorFilter = function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we're
     //         defining. "$(this)" is using jQuery to select that element, so we can chain jQuery methods
     //         onto it.
+    var authorName = $(this).val();
     if ($(this).val()) {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      // estimated time 25 min actual time 40 min
+      $('article').hide();
+
+      $('article[data-author="'+ authorName +'"]').show();
 
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
